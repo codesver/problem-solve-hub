8146f454ca417816983a85534d90d580bd3c26ff
JaeWon Choi <codesver@gmail.com>
2022-11-24 08:48:58 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(reader.readLine());
+        int y = Integer.parseInt(reader.readLine());
+
+        System.out.println(x > 0 ?
+                y > 0 ?
+                        1 :
+                        4 :
+                y > 0 ?
+                        2 :
+                        3);
+    }
+}
