42e3479c1568200d8de4f1d940894d081a483b6d
JaeWon Choi <codesver@gmail.com>
2022-11-23 20:03:48 +0900

    Update 새싹.java

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,12 +1,28 @@
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("         ,r'\"7\n" +
+
+//    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        result.append("         ,r'\"7\n" +
                 "r`-_   ,'  ,/\n" +
                 " \\. \". L_r'\n" +
                 "   `~\\/\n" +
                 "      |\n" +
                 "      |");
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
     }
 }
