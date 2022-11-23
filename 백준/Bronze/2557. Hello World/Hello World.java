a9044769f08114929f749b7e720d145437a9f461
JaeWon Choi <codesver@gmail.com>
2022-11-23 19:14:23 +0900

    Update Hello World.java

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,6 +1,17 @@
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+//    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static void solution() throws IOException {
+        writer.write("Hello World!");
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        writer.flush();
+        writer.close();
     }
 }
