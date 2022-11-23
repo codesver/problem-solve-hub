bad6beba01ab23d0faa833c0f88b66d1744712fa
JaeWon Choi <codesver@gmail.com>
2022-11-24 08:39:38 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int year = Integer.parseInt(reader.readLine());
+        result.append((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0) ? 1 : 0);
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
