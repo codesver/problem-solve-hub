4d5e99de5c18e255b934cd7c518a74bb2244032a
JaeWon Choi <codesver@gmail.com>
2022-11-24 08:50:45 +0900

    Update 사분면 고르기.java

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,19 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
         int x = Integer.parseInt(reader.readLine());
         int y = Integer.parseInt(reader.readLine());
+        result.append(x > 0 ? (y > 0 ? 1 : 4) : (y > 0 ? 2 : 3));
+    }
 
-        System.out.println(x > 0 ?
-                y > 0 ?
-                        1 :
-                        4 :
-                y > 0 ?
-                        2 :
-                        3);
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
     }
 }
