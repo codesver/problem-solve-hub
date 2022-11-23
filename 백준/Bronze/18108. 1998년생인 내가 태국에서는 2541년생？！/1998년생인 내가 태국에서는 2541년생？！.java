62c3503fcc65e7f28fdd884d886efd704ebb33d4
JaeWon Choi <codesver@gmail.com>
2022-11-23 19:36:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        result.append(Integer.parseInt(reader.readLine()) - 543);
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
