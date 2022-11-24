7f511e1778fcfe863ee6c44778e1926b04a7f966
JaeWon Choi <codesver@gmail.com>
2022-11-24 19:10:09 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        String input;
+        while ((input = reader.readLine()) != null) {
+            result.append(Character.getNumericValue(input.charAt(0))
+                    + Character.getNumericValue(input.charAt(2)))
+                    .append("\n");
+        }
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
+
