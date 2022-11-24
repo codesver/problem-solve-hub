b71022183377e890f3af818c030c0602e751600d
JaeWon Choi <codesver@gmail.com>
2022-11-24 14:36:15 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int repeat = Integer.parseInt(reader.readLine());
+        for (int caseNum = 1; caseNum <= repeat; caseNum++) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            result.append("Case #").append(caseNum).append(": ")
+                    .append(Integer.parseInt(tokenizer.nextToken()) + Integer.parseInt(tokenizer.nextToken()))
+                    .append("\n");
+        }
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
