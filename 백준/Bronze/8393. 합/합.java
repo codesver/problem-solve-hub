e3c5b889c11e351a376d96131d4760a654147c6f
JaeWon Choi <codesver@gmail.com>
2022-11-24 13:05:26 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int n = Integer.parseInt(reader.readLine());
+        result.append(n * (n + 1) / 2);
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
