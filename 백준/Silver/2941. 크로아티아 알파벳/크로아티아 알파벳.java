3fd93a2667fc58ca40764e52a77a0ab5a10db7ca
JaeWon Choi <codesver@gmail.com>
2022-11-26 16:03:21 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 120 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        result.append(reader.readLine()
+                .replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=", " ")
+                .length());
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
