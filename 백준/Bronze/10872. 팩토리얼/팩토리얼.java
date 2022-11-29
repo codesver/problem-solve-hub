9f6084eab0b91a7c2887408ebba735e53a1feadb
JaeWon Choi <codesver@gmail.com>
2022-11-29 15:35:40 +0900

    [Bronze V] Title: 팩토리얼, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int N = Integer.parseInt(reader.readLine());
+        result.append(N == 0 ? 1 : IntStream.rangeClosed(1, N).reduce((numA, numB) -> numA * numB).getAsInt());
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
