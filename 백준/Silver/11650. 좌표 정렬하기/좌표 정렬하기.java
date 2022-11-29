b0f3e2817045185bc94757b7eaa56f658c47d5c9
JaeWon Choi <codesver@gmail.com>
2022-11-29 15:04:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 772 ms, Memory: 56584 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int size = Integer.parseInt(reader.readLine());
+        Queue<Point> points = new PriorityQueue<>((o1, o2) -> o1.x != o2.x ? o1.x - o2.x : o1.y - o2.y);
+        while (size-- > 0) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            points.add(new Point(Integer.parseInt(tokenizer.nextToken()), Integer.parseInt(tokenizer.nextToken())));
+        }
+        while (!points.isEmpty()) result.append(points.poll()).append("\n");
+    }
+
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return x + " " + y;
+        }
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
