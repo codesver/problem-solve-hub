14c08b60c1ee43ca90a3094cc4b8c71b35f9eb26
JaeWon Choi <codesver@gmail.com>
2022-12-02 22:05:04 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        String num = "";
+        while (!(num = reader.readLine()).equals("0")) {
+            result.append(String.valueOf(new StringBuilder(num).reverse()).equals(num) ? "yes" : "no")
+                    .append("\n");
+        }
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
