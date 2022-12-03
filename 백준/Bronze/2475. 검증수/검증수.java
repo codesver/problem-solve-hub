049b450c39e864019589d73028327a6bf21edab2
JaeWon Choi <codesver@gmail.com>
2022-12-03 22:06:35 +0900

    [Bronze V] Title: 검증수, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int sum = 0;
+        while (tokenizer.hasMoreTokens()) sum += Math.pow(Integer.parseInt(tokenizer.nextToken()), 2);
+        result.append(sum % 10);
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
