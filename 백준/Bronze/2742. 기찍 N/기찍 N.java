d5772212219184e1fdf3c7ed6d0105c6eaeb74cc
JaeWon Choi <codesver@gmail.com>
2022-12-03 22:17:58 +0900

    [Bronze IV] Title: 기찍 N, Time: 228 ms, Memory: 18916 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int max = Integer.parseInt(reader.readLine());
+        IntStream.rangeClosed(1, max).forEach(num -> result.append(max - num + 1).append("\n"));
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
