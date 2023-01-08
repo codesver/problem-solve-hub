153b37a1dc7a5fb45a97568d5768d23d858ddafe
JaeWon Choi <codesver@gmail.com>
2023-01-09 08:57:36 +0900

    [Silver II] Title: N과 M (12), Time: 180 ms, Memory: 17212 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static int N, M;
+    private static final List<Integer> nums = new ArrayList<>();
+    private static final Deque<Integer> deque = new LinkedList<>();
+    private static final Set<String> set = new HashSet<>();
+
+    private static void solution() throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+        tokenizer = new StringTokenizer(reader.readLine());
+        Set<Integer> set = new HashSet<>();
+        while (tokenizer.hasMoreTokens()) set.add(Integer.parseInt(tokenizer.nextToken()));
+        N = set.size();
+        nums.addAll(set);
+        nums.sort(Comparator.naturalOrder());
+        searchNext(0);
+    }
+
+    private static void searchNext(int start) {
+        IntStream.range(start, N).forEach(idx -> {
+            deque.addLast(nums.get(idx));
+            search(idx);
+            deque.pollLast();
+        });
+    }
+
+    private static void search(int start) {
+        if (deque.size() == M) {
+            IntStream.range(0, M).forEach(len -> {
+                result.append(deque.peekFirst()).append(" ");
+                deque.addLast(deque.pollFirst());
+            });
+            result.append("\n");
+        } else searchNext(start);
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
