48c8f62ed00602e51c597bb3649413d4ac05ceb2
JaeWon Choi <codesver@gmail.com>
2023-01-19 21:08:14 +0900

    [Bronze V] Title: 등록, Time: 124 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        result.append(203).append("\n").append("codesver");
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
