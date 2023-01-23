1d1e2e8776c3c6bd99355f28719c5bcdbc2c3742
JaeWon Choi <codesver@gmail.com>
2023-01-23 20:25:54 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        result.append(Math.abs(Long.parseLong(tokenizer.nextToken())
+                - Long.parseLong(tokenizer.nextToken())));
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
