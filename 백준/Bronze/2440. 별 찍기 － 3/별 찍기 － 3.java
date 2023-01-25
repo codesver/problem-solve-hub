b0c49bfda217d2ae5eea531edd13e10210798f03
JaeWon Choi <codesver@gmail.com>
2023-01-25 10:35:55 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int N = Integer.parseInt(reader.readLine()) + 1;
+        while (--N > 0) result.append("*".repeat(N)).append("\n");
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
