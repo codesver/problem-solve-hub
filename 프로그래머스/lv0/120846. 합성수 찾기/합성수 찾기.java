a485ab49398c323418428bb9690a362c7115cf46
JaeWon Choi <codesver@gmail.com>
2023-01-29 18:38:51 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        boolean[] isNotPrime = new boolean[n + 1];
+        int count = 0;
+        for (int i = 2; i <= n; i++)
+            if (!isNotPrime[i])
+                for (int j = i * 2; j <= n; j += i)
+                    isNotPrime[j] = true;
+            else count++;
+        return count;
+    }
+}
