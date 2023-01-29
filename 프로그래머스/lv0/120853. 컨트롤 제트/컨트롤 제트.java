049e168ad7769e9ec70556d36b689dd5d25bb343
JaeWon Choi <codesver@gmail.com>
2023-01-29 18:38:48 +0900

    [level 0] Title: 컨트롤 제트, Time: 1.34 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,15 +1,12 @@
+import java.util.Stack;
+
 class Solution {
     public int solution(String s) {
-        int sum = 0;
-        int pre = 0;
+        Stack<String> nums = new Stack<>();
         for (String num : s.split(" ")) {
-            if (num.equals("Z")) sum -= pre;
-            else {
-                int n = Integer.parseInt(num);
-                sum += n;
-                pre = n;
-            }
+            if (num.equals("Z")) nums.pop();
+            else nums.push(num);
         }
-        return sum;
+        return nums.stream().mapToInt(Integer::parseInt).sum();
     }
 }
