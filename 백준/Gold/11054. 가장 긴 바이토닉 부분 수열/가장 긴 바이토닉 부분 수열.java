28fea64bd00c4a8b9f9d0216e2958e2405e42392
JaeWon Choi <codesver@gmail.com>
2023-01-30 13:57:01 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 160 ms, Memory: 15224 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder result = new StringBuilder();
+
+    private static void solution() throws IOException {
+        int L = Integer.parseInt(reader.readLine());
+        int[] left = new int[L];
+        int[] right = new int[L];
+        int[] nums = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+
+        for (int i = 1; i < L; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    left[i] = Math.max(left[i], left[j] + 1);
+                }
+            }
+        }
+
+        for (int i = L - 2; i >= 0; i--) {
+            for (int j = L - 1; j > i; j--) {
+                if (nums[j] < nums[i]) {
+                    right[i] = Math.max(right[i], right[j] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < L; i++) max = Math.max(max, left[i] + right[i]);
+        result.append(max - 1);
+    }
+
+    private static void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+        finish();
+    }
+}
