b866e1c0a62e750df53744ccef7a27d70eabc1b1
JaeWon Choi <codesver@gmail.com>
2023-01-31 14:59:36 +0900

    [level 1] Title: [1차] 다트 게임, Time: 2.68 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+
+    Stack<Integer> nums = new Stack<>() {{
+        push(0);
+    }};
+
+    public int solution(String dartResult) {
+        dartResult.replaceAll("10", "+").chars().forEach(ch -> {
+            if (Character.isDigit(ch)) nums.add(Character.getNumericValue(ch));
+            else if (Character.isAlphabetic(ch)) nums.push((int) Math.pow(nums.pop(), ch != 'S' ? ch / 28 : 1));
+            else if (ch == '+') nums.add(10);
+            else if (ch == '#') nums.add(-nums.pop());
+            else if (ch == '*') {
+                int top = nums.pop() * 2;
+                nums.push(nums.pop() * 2);
+                nums.push(top);
+            }
+        });
+        return nums.stream().mapToInt(Integer::intValue).sum();
+    }
+
+}
