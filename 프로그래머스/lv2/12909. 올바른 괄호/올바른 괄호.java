e24100eac1571226779262148715d81cc645e8e7
JaeWon Choi <codesver@gmail.com>
2023-01-31 19:49:47 +0900

    [level 2] Title: 올바른 괄호, Time: 20.56 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,21 +2,17 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        stack.push('t');
-        stack.push(s.charAt(0));
+        Stack<Character> stack = new Stack<>() {{
+            push('t');
+            push(s.charAt(0));
+        }};
 
         for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push('(');
-            } else {
-                Character peek = stack.peek();
-                if (peek == '(')
-                    stack.pop();
-                else
-                    return false;
-            }
+            if (s.charAt(i) == '(') stack.push('(');
+            else if (stack.peek() == '(') stack.pop();
+            else return false;
         }
+        
         return stack.size() == 1;
     }
 }
