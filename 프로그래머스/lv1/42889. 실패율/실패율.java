7920b70733873f97b732b024447056f5a3a5a3f7
JaeWon Choi <codesver@gmail.com>
2023-01-31 16:22:58 +0900

    [level 1] Title: 실패율, Time: 19.80 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,43 @@
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int N, int[] S) {
+        return IntStream
+                .rangeClosed(1, N)
+                .mapToObj(Stage::new)
+                .collect(Collectors.toList())
+                .stream()
+                .peek(stage -> stage.update(S))
+                .peek(Stage::calcAvg)
+                .sorted(Comparator.comparingDouble(s -> -s.average))
+                .mapToInt(Stage::getNum).toArray();
+    }
+}
+
+class Stage {
+
+    int num;
+    int arrive = 0, trial = 0;
+    double average;
+
+    public Stage(int num) {
+        this.num = num;
+    }
+
+    public void update(int[] S) {
+        for (int s : S) {
+            if (s >= num) arrive++;
+            if (s == num) trial++;
+        }
+    }
+
+    public void calcAvg() {
+        average = arrive == 0 ? 0 : trial / (double) arrive;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
