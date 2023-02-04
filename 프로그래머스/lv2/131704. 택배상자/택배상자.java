ed53c44a4745d63e5f7f3a1bd16db31033c7091f
JaeWon Choi <codesver@gmail.com>
2023-02-04 17:41:25 +0900

    [level 2] Title: 택배상자, Time: 193.93 ms, Memory: 169 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] order) {
+        Queue<Integer> main = IntStream.rangeClosed(1, order.length)
+                .boxed().collect(Collectors.toCollection(LinkedList::new));
+        Stack<Integer> sub = new Stack<>();
+
+        return (int) Arrays.stream(order).takeWhile(box -> {
+            if (!sub.isEmpty() && sub.peek() == box) return sub.pop() > 0;
+            else while (!main.isEmpty() && main.peek() <= box)
+                if (main.peek() == box) return main.poll() > 0;
+                else sub.push(main.poll());
+            return false;
+        }).count();
+    }
+}
