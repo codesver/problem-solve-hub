e82963e6b4ce6dc649a596ba7a857bd7e27eba78
JaeWon Choi <codesver@gmail.com>
2023-02-10 13:39:47 +0900

    [level 2] Title: n^2 배열 자르기, Time: 7.61 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=java) 
 
 ### 성능 요약
 
-메모리: 127 MB, 시간: 576.30 ms
+메모리: 109 MB, 시간: 7.61 ms
 
 ### 구분
 
--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,10 +1,9 @@
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.stream.LongStream;
 
 class Solution {
     public int[] solution(int n, long left, long right) {
-        return IntStream.rangeClosed((int) (left / n + 1), (int) (right / n + 1))
-                .flatMap(i -> Stream.of(IntStream.generate(() -> i).limit(i), IntStream.rangeClosed(i + 1, n)).flatMapToInt(j -> j))
-                .skip(left % n).limit(right - left + 1).toArray();
+        return LongStream.rangeClosed(left, right)
+                .mapToInt(e -> (int) (Math.max(e / n, e % n) + 1))
+                .toArray();
     }
 }
