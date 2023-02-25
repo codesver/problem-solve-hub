18dee64143567bdbd037ddaeb8d10184e24331c7
JaeWon Choi <codesver@gmail.com>
2023-02-25 09:40:54 +0900

    [Bronze V] Title: 카드 게임, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+    public final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public final StringBuilder result = new StringBuilder();
+
+    public void solve() throws IOException {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) sum += Integer.parseInt(reader.readLine());
+        result.append(sum);
+    }
+
+    public void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solve();
+        main.finish();
+    }
+}
