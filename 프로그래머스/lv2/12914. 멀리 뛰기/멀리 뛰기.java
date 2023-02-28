414ad140cb966c608f15e1d82ab63aaf89b77d77
JaeWon Choi <codesver@gmail.com>
2023-02-28 10:30:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int n) {
+        if (n == 1) return 1;
+        else {
+            int[] dp = new int[n + 1];
+            dp[2] = (dp[1] = 1) + 1;
+            for (int i = 3; i <= n; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+            return dp[n];
+        }
+    }
+}
