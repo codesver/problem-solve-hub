0c52963488eb0cf415d359291d82b48551bbf380
JaeWon Choi <codesver@gmail.com>
2023-03-03 10:09:54 +0900

    [Gold IV] Title: 스도쿠, Time: 348 ms, Memory: 18192 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,6 +1,10 @@
+import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
+
 
 public class Main {
 
@@ -8,43 +12,50 @@ public class Main {
     private final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
     private final StringBuilder result = new StringBuilder();
 
-    private final int[][] initBoard = new int[9][9], board = new int[9][9];
-    private final int[][] moves = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    private final List<Point> blanks = new ArrayList<>();
+    private final int[][] board = new int[9][9];
+    private final int[] rowNums = new int[9], colNums = new int[9];
+    private final int[][] blockNums = new int[3][3];
 
     public void solve() throws IOException {
         init();
-        search(0, 0);
+        search(0);
         result.append(Arrays.stream(board).map(row -> Arrays.stream(row).mapToObj(String::valueOf).collect(Collectors.joining()) + "\n").collect(Collectors.joining()));
     }
 
     private void init() throws IOException {
         for (int r = 0; r < 9; r++) {
             int[] row = Arrays.stream(reader.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-            for (int c = 0; c < 9; c++) initBoard[r][c] = board[r][c] = row[c];
+            for (int c = 0; c < 9; c++) {
+                if ((board[r][c] = row[c]) != 0) {
+                    int visitBit = 1 << board[r][c] - 1;
+                    rowNums[r] |= visitBit;
+                    colNums[c] |= visitBit;
+                    blockNums[r / 3][c / 3] |= visitBit;
+                } else blanks.add(new Point(r, c));
+            }
         }
     }
 
-    private boolean search(int row, int col) {
-        if (initBoard[row][col] != 0) return (row + col == 16 || search(col + 1 == 9 ? row + 1 : row, (col + 1) % 9));
-        else for (int n = 1; n <= 9; n++) {
-            board[row][col] = n;
-            if (validation(row, col) && (row + col == 16 || search(col + 1 == 9 ? row + 1 : row, (col + 1) % 9)))
-                return true;
+    private boolean search(int index) {
+        if (index == blanks.size()) return true;
+        int row = blanks.get(index).x, col = blanks.get(index).y;
+        int visit = rowNums[row] | colNums[col] | blockNums[row / 3][col / 3];
+
+        for (int num = 1; num <= 9; num++) {
+            int bit = 1 << (num - 1);
+            if ((visit & bit) != 0) continue;
+            board[row][col] = num;
+            rowNums[row] |= bit;
+            colNums[col] |= bit;
+            blockNums[row / 3][col / 3] |= bit;
+            if (search(index + 1)) return true;
+            board[row][col] = 0;
+            rowNums[row] &= ~bit;
+            colNums[col] &= ~bit;
+            blockNums[row / 3][col / 3] &= ~bit;
         }
-        board[row][col] = 0;
         return false;
-    }
-
-    private boolean validation(int row, int col) {
-        for (int rc = 0; rc < 9; rc++) {
-            if (board[rc][col] == board[row][col] && rc != row) return false;
-            if (board[row][rc] == board[row][col] && rc != col) return false;
-        }
-        int r = row / 3 * 3 + 1, c = col / 3 * 3 + 1;
-        for (int m = 0; m < 9; m++)
-            if (board[r + moves[m][0]][c + moves[m][1]] == board[row][col] && !(row == r + moves[m][0] && col == c + moves[m][1]))
-                return false;
-        return true;
     }
 
     private void finish() throws IOException {
