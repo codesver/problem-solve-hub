e9595bd6a6164d4e85964501052062a390ab2c34
JaeWon Choi <codesver@gmail.com>
2023-03-07 10:45:31 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.13 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+
+    private final int[] DISCOUNT_VALUES = new int[]{10, 20, 30, 40};
+    private final Stack<Integer> discounts = new Stack<>();
+
+    private int[][] users;
+    private int[] emoticons;
+    private int maxSubs = 0, maxProfit = 0;
+
+    public int[] solution(int[][] users, int[] emoticons) {
+        this.users = users;
+        this.emoticons = emoticons;
+        search();
+        return new int[]{maxSubs, maxProfit};
+    }
+
+    private void search() {
+        if (emoticons.length == discounts.size()) {
+            int subs = 0, profit = 0;
+            for (int[] user : users) {
+                int payment = 0;
+                for (int i = 0; i < emoticons.length; i++)
+                    if (discounts.get(i) >= user[0])
+                        payment += emoticons[i] * (100 - discounts.get(i)) / 100;
+                if (payment >= user[1]) subs++;
+                else profit += payment;
+            }
+            if (subs > maxSubs) {
+                maxSubs = subs;
+                maxProfit = profit;
+            } else if (subs == maxSubs && profit > maxProfit) maxProfit = profit;
+        } else for (int d = 0; d < 4; d++) {
+            discounts.push(DISCOUNT_VALUES[d]);
+            search();
+            discounts.pop();
+        }
+    }
+}
