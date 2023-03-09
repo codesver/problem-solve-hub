8884d8f8886b9eeef87ade89f841857c5d17a41a
JaeWon Choi <codesver@gmail.com>
2023-03-09 11:12:03 +0900

    [unrated] Title: 디펜스 게임, Time: 0.74 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -5,14 +5,14 @@ import java.util.Queue;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         Queue<Integer> enemies = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int i = 0; i < enemy.length; i++) {
-            int e = enemy[i];
+        for (int round = 1; round <= enemy.length; round++) {
+            int e = enemy[round - 1];
             n -= e;
             enemies.offer(e);
             if (n < 0 && k > 0 && !enemies.isEmpty()) {
                 n += enemies.poll();
                 k--;
-            } else if (n < 0) return i;
+            } else if (n < 0) return round - 1;
         }
         return enemy.length;
     }
