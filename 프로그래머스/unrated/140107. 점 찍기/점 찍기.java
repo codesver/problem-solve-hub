ca7e86e6fd16058b68fe0571109430b78f8c176d
JaeWon Choi <codesver@gmail.com>
2023-03-10 10:40:54 +0900

    [unrated] Title: 점 찍기, Time: 3.11 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
@@ -2,7 +2,7 @@ import java.util.stream.LongStream;
 
 class Solution {
     public long solution(int k, int d) {
-        return LongStream.rangeClosed(0, d).filter(x -> x % k == 0)
+        return LongStream.iterate(0, x -> x <= d, x -> x + k)
                 .map(x -> (int) Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2)) / k + 1).sum();
     }
 }
