3acc9f84d6fc25664a01c0d49defe34a5fe7e59a
JaeWon Choi <codesver@gmail.com>
2023-03-11 11:21:41 +0900

    [Gold III] Title: 소수의 연속합, Time: 220 ms, Memory: 21716 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private final StringBuilder result = new StringBuilder();
+
+    public void solve() throws IOException {
+        final int NUM = Integer.parseInt(reader.readLine());
+        if (NUM == 1) result.append(0);
+        else {
+            boolean[] isNotPrime = new boolean[NUM + 1];
+            for (int n = 2; n <= NUM; n++)
+                if (!isNotPrime[n])
+                    for (int ne = n + n; ne <= NUM; ne += n) isNotPrime[ne] = true;
+            int[] primes = IntStream.rangeClosed(2, NUM).filter(num -> !isNotPrime[num]).toArray();
+            int front = 0, back = 1, sum = primes[0], count = 0;
+            while (true) {
+                if (sum >= NUM) {
+                    if (sum == NUM) count++;
+                    if (front + 1 == back) break;
+                    sum -= primes[front++];
+                } else {
+                    if (back == primes.length) break;
+                    sum += primes[back++];
+                }
+            }
+            result.append(count);
+        }
+    }
+
+    private void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solve();
+        main.finish();
+    }
+}
