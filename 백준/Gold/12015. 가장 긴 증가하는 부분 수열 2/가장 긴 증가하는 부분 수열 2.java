47114a8e21b8155d28bfb5ab810fa6404283528d
JaeWon Choi <codesver@gmail.com>
2023-03-20 08:26:07 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 708 ms, Memory: 115456 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private final StringBuilder result = new StringBuilder();
+
+    public void solve() throws IOException {
+        int N = Integer.parseInt(reader.readLine());
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        List<Integer> list = new ArrayList<>(Collections.singletonList(0));
+        for (int n = 1; n <= N; n++) {
+            int num = Integer.parseInt(tokenizer.nextToken());
+            if (list.get(list.size() - 1) < num) list.add(num);
+            else binarySearch(list, num);
+        }
+
+        result.append(list.size() - 1);
+    }
+
+    private void binarySearch(List<Integer> list, int num) {
+        int left = 0, right = list.size() - 1, mid;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (list.get(mid) >= num) right = mid;
+            else left = mid + 1;
+        }
+        list.set(right, num);
+    }
+
+    private void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solve();
+        main.finish();
+    }
+}
