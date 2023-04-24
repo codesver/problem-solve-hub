cfc900dd38d531c26f7613f99427cde97f9433aa
JaeWon Choi <codesver@gmail.com>
2023-04-24 14:10:00 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 13.35 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.kt
@@ -0,0 +1,5 @@
+class Solution {
+    fun solution(arr: IntArray, queries: Array<IntArray>): IntArray = arr.apply {
+        queries.forEach { (s, e, k) -> (s..e).filter { it % k == 0 }.forEach { arr[it]++ } }
+    }
+}
