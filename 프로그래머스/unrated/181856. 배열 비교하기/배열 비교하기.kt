67b6d81dda8cc9b5cf77fcb7a2d924a066cc2720
JaeWon Choi <codesver@gmail.com>
2023-04-25 17:34:55 +0900

    [unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.kt
@@ -0,0 +1,5 @@
+class Solution {
+    fun solution(arr1: IntArray, arr2: IntArray) =
+        if (arr1.size == arr2.size) arr1.sum().compareTo(arr2.sum())
+        else arr1.size.compareTo(arr2.size)
+}
