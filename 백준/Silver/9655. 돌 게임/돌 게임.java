fee57d073d25028dfc4fca5611a8fc49cf7fcfd0
JaeWon Choi <codesver@gmail.com>
2023-05-22 08:30:19 +0900

    [Silver V] Title: 돌 게임, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private final StringBuilder result = new StringBuilder();
+
+    public void solve() throws IOException {
+        result.append(Integer.parseInt(reader.readLine()) % 2 == 1 ? "SK" : "CY");
+    }
+
+    public void finish() throws IOException {
+        writer.write(result.toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solve();
+        main.finish();
+    }
+}
