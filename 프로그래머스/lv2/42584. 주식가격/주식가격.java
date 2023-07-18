96422d9048a83bc88dc68379a57d4224c0551b59
JaeWon Choi <codesver@gmail.com>
2023-07-18 17:07:49 +0900

    [level 2] Title: 주식가격, Time: 25.52 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,39 +1,19 @@
-import java.util.Queue;
-import java.util.PriorityQueue;
+import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] prices) {
         int[] times = new int[prices.length];
-        
-        Queue<Price> queue = new PriorityQueue<>((pa, pb) -> {
-            if (pa.cost == pb.cost) return pa.time - pb.time;
-            else return pb.cost - pa.cost;
-        });
-            
-        for (int p = 0; p < prices.length; p++) {
-            Price price = new Price(p, prices[p]);
-            while(!queue.isEmpty() && queue.peek().cost > price.cost) {
-                Price prePrice = queue.poll();
-                times[prePrice.time] = price.time - prePrice.time;
-            }
-            queue.offer(price);
-        }
-        
-        while (!queue.isEmpty()) {
-            Price price = queue.poll();
-            times[price.time] = prices.length - price.time - 1;
-        }
-        
-        return times;
-    }
-}
 
-class Price {
-    
-    int time, cost;
-    
-    public Price(int time, int cost) {
-        this.time = time;
-        this.cost = cost;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int time = 0; time < prices.length; time++) {
+            while (!stack.isEmpty() && prices[time] < prices[stack.peek()])
+                times[stack.peek()] = time - stack.pop();
+            stack.push(time);
+        }
+
+        while (!stack.isEmpty()) times[stack.peek()] = prices.length - stack.pop() - 1;
+
+        return times;
     }
 }
